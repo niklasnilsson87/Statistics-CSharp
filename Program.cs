58ea4d2f444bcffc550febafdfb3f870f618c310
Niklas Nilsson <niklas.nilsson1987@gmail.com>
2019-03-29 14:48:58 +0100

    started working on ex1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace examination_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
